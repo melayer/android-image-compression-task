2ed178ff1614cc4ff436b32a5ba64e715a579538
Aniruddha Avinash Kudalkar <aniruddha.kudalkar@gmail.com>
2016-06-15 20:07:26 +0530

    Update ImageCompression.java

--- ImageCompression.java
+++ ImageCompression.java
@@ -3,6 +3,27 @@ public class ImageCompressionAsyncTask extends AsyncTask<String, Void, String> {
         public ImageCompressionAsyncTask() {
         }
 
+        public int calculateInSampleSize(BitmapFactory.Options options,
+                                     int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        final float totalPixels = width * height;
+        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++;
+        }
+
+        return inSampleSize;
+    }
+
         @Override
         protected String doInBackground(String... params) {
             String filePath = "";
